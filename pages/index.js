2c784aa98cc0d0518eeb7e37d443194b06f21de6
Christopher J Lyth <cjlyth@gmail.com>
2018-06-05 06:39:49 -0500

    Initial add of flow with supporting files.

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
+// @flow
 /* eslint react/prefer-stateless-function: 0 */
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+
 import Head from 'next/head';
 
 import withAuth from '../lib/withAuth';
 import withLayout from '../lib/withLayout';
+import { User as UserType } from '../lib/types';
 
-class Index extends React.Component {
-  static propTypes = {
-    user: PropTypes.shape({
-      email: PropTypes.string.isRequired,
-    }),
-  };
 
+type Props = {
+  user: UserType
+};
+
+class Index extends PureComponent<Props> {
   static defaultProps = {
     user: null,
   };
