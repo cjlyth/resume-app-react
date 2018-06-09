c3d52d2f76f3c5831cb15cec68072b2abb29252d
Christopher J Lyth <cjlyth@gmail.com>
2018-06-09 13:29:56 -0500

    WIP Summary component  - Got the grid centered  - formatted the name and title text  - responsive text values

--- app/summary/summary.jsx
+++ app/summary/summary.jsx
@@ -2,15 +2,23 @@
 import React, { PureComponent } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Hidden from '@material-ui/core/Hidden';
 
 import config from '../../lib/config';
-
+import { styleSummaryAvatar } from '../../lib/SharedStyles';
 import type { Summary as SummaryType } from '../../lib/types';
 
 type Props = {
   +fetchSummary: Function,
   +summary: SummaryType
 }
+
+const styles = {
+  center: {
+    textAlign: 'center',
+  },
+};
 
 class Summary extends PureComponent<Props> {
   componentDidMount() {
@@ -22,27 +30,45 @@ class Summary extends PureComponent<Props> {
       fetchSummary,
       summary,
     } = this.props;
-    const { name, title } = summary.data;
+    const { name, nameSmall, title, titleSmall } = summary.data;
     return (
-      <div>
-        <Grid container direction="row" justify="space-around" alignItems="center">
-          <Grid item xs={12}>
-            {summary.data.links
+      <Grid container direction="column" justify="space-around" alignItems="center">
+        <Grid item xs={4} sm={3} md={2} >
+          {summary.data.links
             .filter(link => link.rel === 'avatar')
             .map(({ href }) => (
               <Avatar
+                style={styleSummaryAvatar}
                 key={`${name}-logo`}
                 src={`${config.app.resumeDataAPIUrl}/${href}`}
                 alt={name}
               />
             ))}
-          </Grid>
-          <Grid item xs={12}>{name}</Grid>
-          <Grid item xs={12}>{title}</Grid>
         </Grid>
-        <pre>{JSON.stringify(summary, undefined, 2)}</pre>
-        <button onClick={fetchSummary}>Load</button>
-      </div>
+        <Grid item xs={12} alignItems="center">
+          <Hidden xsDown>
+            <Typography variant="title" style={styles.center}>
+              {name}
+            </Typography>
+            <Typography variant="subheading" style={styles.center}>
+              {title}
+            </Typography>
+          </Hidden>
+          <Hidden smUp>
+            <Typography variant="title" style={styles.center}>
+              {nameSmall}
+            </Typography>
+            <Typography variant="subheading" style={styles.center}>
+              {titleSmall}
+            </Typography>
+          </Hidden>
+
+        </Grid>
+        <Grid xs={12}>
+          <button onClick={fetchSummary}>Load</button>
+          <pre>{JSON.stringify(summary, undefined, 2)}</pre>
+        </Grid>
+      </Grid>
     );
   }
 }
