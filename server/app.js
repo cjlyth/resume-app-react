cad9c80d91dbf65065e027c17251d5b3f14764db
Christopher J Lyth <cjlyth@gmail.com>
2018-06-10 10:07:12 -0500

    Cleaned up the app.js script to remove a hard coded domain introduced a new environment variable "ROOT_URL"

--- server/app.js
+++ server/app.js
@@ -3,22 +3,18 @@ const next = require('next');
 require('dotenv').config();
 
 const dev = process.env.NODE_ENV !== 'production';
-console.log('dev', dev);
 
 const port = process.env.PORT || 8000;
-const ROOT_URL = dev ? `http://localhost:${port}` : 'https://mydomain.com';
+const ROOT_URL = process.env.ROOT_URL || `http://localhost:${port}`;
 
-const app = next({ dev, quiet: false });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
-// Nextjs's server prepared
 app.prepare().then(() => {
   const server = express();
 
-  // TODO: Add API for resume data
   server.get('*', (req, res) => handle(req, res));
 
-  // starting express server
   server.listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on ${ROOT_URL}`); // eslint-disable-line no-console
