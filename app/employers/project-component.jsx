5af7c2338da8a85628788ea87c21c74db3cde1a0
Christopher J Lyth <cjlyth@gmail.com>
2018-06-12 03:19:43 -0500

    Made a component to loop over array typed stuff for debugging

--- app/employers/project-component.jsx
+++ app/employers/project-component.jsx
@@ -1,11 +1,15 @@
 // @flow
 import React, { PureComponent } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import fp from 'lodash/fp';
+import shortid from 'shortid';
+
 import type { Project as ProjectType } from '../../lib/types/index';
 import { DateRange } from './utils';
 
@@ -26,19 +30,43 @@ const styles = theme => ({
 type Props = ProjectType & {
   classes: Object,
 }
+
+
+const arrayKeys = [
+  'roles',
+  'objective',
+  'responsibilities',
+  'technologies',
+  'achievements',
+];
+
+const toGridItem = (props, key) => (
+  <Grid item key={shortid.generate()}>
+    <Typography>{fp.startCase(key)}</Typography>
+    <pre>{JSON.stringify(props[key], null, 2)}</pre>
+  </Grid>
+);
+
 class ProjectComponent extends PureComponent<Props> {
   render() {
-    const { longName, dates, classes } = this.props;
+    const {
+      longName,
+      dates,
+      classes,
+      ...data
+    } = this.props;
     return (
       <ExpansionPanel>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
           <Typography className={classes.heading}>{longName} </Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <div>
-            <Typography className={classes.subheading}><DateRange dates={dates} /> </Typography>
-          </div>
-          <pre>{JSON.stringify(this.props, null, 2)}</pre>
+          <Grid>
+            <Grid item>
+              <Typography><DateRange dates={dates} /></Typography>
+            </Grid>
+            {arrayKeys.map(k => toGridItem(data, k))}
+          </Grid>
         </ExpansionPanelDetails>
       </ExpansionPanel>
     );
