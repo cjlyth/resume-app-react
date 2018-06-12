2882065ef357c08c55c8087d34aa8918921fafca
Christopher J Lyth <cjlyth@gmail.com>
2018-06-11 23:21:35 -0500

    Removed unused imports in employer.jsx

--- app/employers/employer.jsx
+++ app/employers/employer.jsx
@@ -7,15 +7,10 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
 import SvgIcon from '@material-ui/core/SvgIcon';
 
-import Typography from '@material-ui/core/Typography';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
 import LinkIcon from '@material-ui/icons/Link';
 
-import config from '../../lib/config';
 import EmployerAvatar from './employer-avatar';
 import EmployerContent from './employer-content';
 import EmployerAction from './employer-action';
