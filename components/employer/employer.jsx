5d89e1d334aa8f66ba99d1fbd2155d593b574d4b
Christopher J Lyth <cjlyth@gmail.com>
2018-06-17 21:54:04 -0500

    Not showing employers for which I have no projects

--- components/employer/employer.jsx
+++ components/employer/employer.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import sid from 'shortid';
 import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 import Typography from '@material-ui/core/Typography';
 import type { ComponentType } from 'react';
 import type {
@@ -16,20 +17,25 @@ type Props = EmployerType & {
 
 const Employer = ({
   name, website, projects, ProjectComponent,
-}:Props) => (
-  <Grid item key={website} lg={10}>
-    <Typography variant="subheading" paragraph>{name}
-      <Typography variant="caption" gutterBottom>{website}</Typography>
-    </Typography>
-    {projects
-      .filter(project => project.employerWebsite === website)
-      .map(project => (
-        <ProjectComponent
-          key={sid.generate()}
-          {...project}
-        />
-      ))
-    }
-  </Grid>
-);
+}:Props) => {
+  const employerProjects: Array<ProjectType> = projects
+    .filter(project => project.employerWebsite === website);
+
+  return (
+    <Hidden xsUp={employerProjects.length < 1}>
+      <Grid item key={website} lg={10}>
+        <Typography variant="subheading" paragraph>{name}
+          <Typography variant="caption" gutterBottom>{website}</Typography>
+        </Typography>
+        {employerProjects.map(project => (
+            <ProjectComponent
+              key={sid.generate()}
+              {...project}
+            />
+          ))}
+      </Grid>
+    </Hidden>
+  );
+}
+
 export default Employer;
