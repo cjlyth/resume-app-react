6b68ac19404e0106579220466d16eb39226fdac7
Christopher J Lyth <cjlyth@gmail.com>
2018-06-24 12:26:46 -0500

    Have redux managing the settings toggle

--- app/summary/summary-gql-container.js
+++ app/summary/summary-gql-container.js
@@ -2,12 +2,27 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-
+import { connect } from 'react-redux';
 import Summary from '../../components/summary';
 import utils from '../utils';
 import { Loading, Error } from '../loading';
+import { toggleSettings } from '../settings/settings-actions';
 
-const SummaryData = () => (
+
+const mapDispatchToProps = dispatch => ({
+  toggleSettings: () => {
+    dispatch(toggleSettings());
+  },
+  fetchSummary: () => {},
+});
+
+function mapStateToProps(state) {
+  const { settings } = state;
+  const { settingsOpen } = settings;
+  return { settingsOpen };
+}
+
+const SummaryData = props => (
   <Query
     query={gql`
       {
@@ -32,10 +47,15 @@ const SummaryData = () => (
           avatarUrl={getLink('avatar')}
           linkedInUrl={getLink('linkedin')}
           {...rest}
-          toggleSettings={() => {}}
+          {...props}
         />
       );
     }}
   </Query>
 );
-export default SummaryData;
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(SummaryData);
